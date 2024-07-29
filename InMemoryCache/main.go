44f1ed0033ba6bf247d07921035312ee3693e63c
Keigo-Hirohara <hirokei.camel@gmail.com>
2024-07-30 08:44:20 +0900

    add: インメモリでキャッシュを実装

---
+++ InMemoryCache/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"log"
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	mu    sync.Mutex
+	items map[int]int
+}
+
+func NewCache() *Cache {
+	m := make(map[int]int)
+	c := &Cache{
+		items: m,
+	}
+	return c
+}
+
+func (c *Cache) Set(key int, value int) {
+	c.mu.Lock()
+	c.items[key] = value
+	c.mu.Unlock()
+}
+
+func (c *Cache) Get(key int) int {
+	c.mu.Lock()
+	v, ok := c.items[key]
+	c.mu.Unlock()
+
+	if ok {
+		return v
+	}
+
+	v = HeavyGet(key)
+
+	c.Set(key, v)
+
+	return v
+}
+
+// キャッシュに値がなかった場合の関数
+// 実際にはデータベースへのアクセスなどが発生
+// 今回は仮にkeyの2倍の値を返す
+func HeavyGet(key int) int {
+	time.Sleep(time.Second)
+	return key * 2
+}
+
+func main() {
+	mCache := NewCache()
+	log.Println(mCache.Get(3))
+}
